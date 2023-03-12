83df442c4418586288b94aa1c30eae036d5e205c
Siya Choudhary <67403730+siyachoudhary@users.noreply.github.com>
2023-03-11 23:28:14 -0600

    user creation and password hashing backend complete

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,12 @@
+const bcrypt = require("bcrypt");
+
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+
 const dbConnect = require("./db/dbConnect");
+
+const User = require("./db/userModel");
 
 dbConnect();
 
@@ -12,6 +17,45 @@ app.use(bodyParser.json());
 app.get("/", (request, response, next) => {
   response.json({ message: "Hey! This is your server response!" });
   next();
+});
+
+
+// register endpoint
+app.post("/register", (request, response) => {
+  // hash the password
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      // create a new user instance and collect the data
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      });
+
+      // save the new user
+      user.save()
+        // return success if the new user is added to the database successfully
+        .then((result) => {
+          response.status(201).send({
+            message: "User Created Successfully",
+            result,
+          });
+        })
+        // catch error if the new user wasn't added successfully to the database
+        .catch((error) => {
+          response.status(500).send({
+            message: error.message,
+            error,
+          });
+        });
+    })
+    // catch error if the password hash isn't successful
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
 });
 
 
